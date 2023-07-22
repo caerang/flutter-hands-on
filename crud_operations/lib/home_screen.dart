5d29b463e5b31b90f2a46b59a386cc4ef40a19ae
Hyeongdo Lee <mylovercorea@gmail.com>
2023-07-22 20:38:26 +0900

    Add CRUD operations hands-on
    https://www.youtube.com/watch?v=jnkItI-0Fn0

---
+++ crud_operations/lib/home_screen.dart
@@ -0,0 +1,170 @@
+import 'package:crud_operations/db_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> _allData = [];
+  bool _isLoading = true;
+
+  void _refreshData() async {
+    final data = await SQLHelper.getAllData();
+    setState(() {
+      _allData = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  Future<void> _addData() async {
+    await SQLHelper.createData(_titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  Future<void> _updateData(int id) async {
+    await SQLHelper.updateData(id, _titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  void _deleteData(int id) async {
+    await SQLHelper.deleteData(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        backgroundColor: Colors.redAccent, content: Text("Data Deleted")));
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  void showBottomSheet(int? id) async {
+    if (id != null) {
+      final existingData =
+          _allData.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingData['title'];
+      _descController.text = existingData['desc'];
+    }
+    showModalBottomSheet(
+      elevation: 5,
+      isScrollControlled: true,
+      context: context,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          top: 30,
+          left: 15,
+          right: 15,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 50,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Title",
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+                controller: _descController,
+                maxLines: 4,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Description")),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (id == null) {
+                    await _addData();
+                  }
+                  if (id != null) {
+                    await _updateData(id);
+                  }
+
+                  _titleController.text = "";
+                  _descController.text = "";
+
+                  Navigator.of(context).pop();
+                  print("Data Added");
+                },
+                child: Padding(
+                    padding: const EdgeInsets.all(18),
+                    child: Text(
+                      id == null ? "Add Data" : "Update",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFECEAF4),
+      appBar: AppBar(
+        title: const Text("CRUD Operations"),
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _allData.length,
+              itemBuilder: (context, index) => Card(
+                margin: const EdgeInsets.all(15),
+                child: ListTile(
+                    title: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Text(_allData[index]['title'],
+                            style: const TextStyle(fontSize: 20))),
+                    subtitle: Text(_allData[index]['desc']),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              showBottomSheet(_allData[index]['id']);
+                            },
+                            icon: const Icon(
+                              Icons.edit,
+                              color: Colors.indigo,
+                            )),
+                        IconButton(
+                            onPressed: () {
+                              _deleteData(_allData[index]['id']);
+                            },
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.redAccent,
+                            )),
+                      ],
+                    )),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showBottomSheet(null),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ crud_operations/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:crud_operations/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
